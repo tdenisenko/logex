d9f4f8bc4b18932ec32988e26ff10a361f5e7eca
Tim Denisenko <timothy243@gmail.com>
2026-04-08 21:17:51 +0700

    remove dead code and unused dependencies

--- crates/logex-types/src/lib.rs
+++ crates/logex-types/src/lib.rs
@@ -1,11 +1,7 @@
-mod column;
-mod error;
 mod log_row;
 mod partition;
 mod sync;
 
-pub use column::ColumnId;
-pub use error::LogExError;
 pub use log_row::{BlockContext, LogRow, Source};
 pub use partition::PartitionMeta;
 pub use sync::{NodeState, SyncStatus};
